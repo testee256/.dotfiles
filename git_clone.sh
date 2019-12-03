c651311d726faaedd53722c28401ba05bca9432e
Jun Huang <tristar.huang@gmail.com>
2019-12-02 22:33:25 -0800

    updaeted vimrc； changed ff to unix

---
+++ git_clone.sh
@@ -0,0 +1,19 @@
+# update autocrlf setting
+git config --global core.autocrlf false
+git config --global core.safecrlf true
+git config --global core.eol lf
+
+# manual installation for cygwin
+git clone https://github.com/tristar2001/vim-cide
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
+git clone https://github.com/godlygeek/tabular
+git clone https://github.com/vim-scripts/taglist.vim
+git clone https://github.com/plasticboy/vim-markdown
+git clone https://github.com/fholgado/minibufexpl.vim
+git clone https://github.com/nanotech/jellybeans.vim
+git clone https://github.com/Yggdroot/indentLine
+
+# install font to mintty
+https://superuser.com/questions/887300/how-to-install-a-font-onto-cygwin-terminal
+
