b04c2fbb2a4dea145079c33985969a742195783f
Jun Huang <tristar.huang@gmail.com>
2020-04-14 21:11:19 -0700

    added vm_inst.sh

--- docs/vb_inst.sh
+++ docs/vb_inst.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/sh
-sudo apt update # Fetches the list of available updates
-sudo apt install build-essential linux-headers-$(uname -r) dkms
+#!/bin/bash
+apt update # Fetches the list of available updates
+apt install build-essential linux-headers-$(uname -r) dkms
 echo Please select "Device->Insert Guest Addition ISO", and click "Run"
-read -rsp 'Press <ENTER> to continue ...\n'
+read -rsp 'Press ENTER to continue ...'
 lsmod | grep guest
-sudo usermod -G vboxsf -a $USER
-read -rsp 'Installation of Guest Addition is complete; Press <enter> to shutdown ...\n'
-sudo shutdown -P now
+echo ''
+usermod -G vboxsf -a $USER
+read -rsp 'Installation of Guest Addition is completed; Press ENTER to shutdown ...'
+echo ''
+shutdown -P now
 
