6a35228214276b321fd7c7658350fc456b600d6a
Jun Huang <tristar.huang@gmail.com>
2020-04-14 21:33:07 -0700

    fixed vm_inst.sh

--- docs/vm_inst.sh
+++ docs/vm_inst.sh
@@ -3,9 +3,16 @@ apt update # Fetches the list of available updates
 apt install build-essential linux-headers-$(uname -r) dkms
 echo Please select "Device->Insert Guest Addition ISO", and click "Run"
 read -rsp 'Press ENTER to continue ...'
-lsmod | grep guest
 echo ''
-usermod -G vboxsf -a $USER
+lsmod | grep guest
+
+if [[ -z "${SUDO_USER}" ]]; then
+  USER_REAL=${USER}
+else
+  USER_REAL=${SUDO_USER}
+fi
+echo Adding $USER_REAL to group vboxsf ...
+usermod -G vboxsf -a $USER_REAL
 read -rsp 'Installation of Guest Addition is completed; Press ENTER to shutdown ...'
 echo ''
 shutdown -P now
